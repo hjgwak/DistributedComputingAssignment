eeb41a2f3a04cc2952d3397e89988f52f8ba2ab1
hjgwak <khj2830@gmail.com>
2016-11-08 01:12:14 +0900

    get HTTP from qotd.
    get HTTP from URL

--- rmi-GwakHoJin/src/server/XotdServiceImpl.java
+++ rmi-GwakHoJin/src/server/XotdServiceImpl.java
@@ -2,24 +2,71 @@ package server;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import xotd.XotdService;
 
 public class XotdServiceImpl extends UnicastRemoteObject implements XotdService {
+	public static final long serialVersionUID = 1L;
+	public final String xotd_url = "http://www.quotationspage.com/";
+	
 	public XotdServiceImpl() throws RemoteException { 
 		super(); 
 	}
 	
 	@Override
 	public String getQotd() throws RemoteException {
-		// TODO Auto-generated method stub
-		return "Qotd";
+		String urlPath = xotd_url + "qotd.html";
+		String pageContents = "";
+		StringBuilder contents = new StringBuilder();
+		
+		System.out.println("[RMI-hjgwak-server] URL : " + urlPath);
+		try {
+			URL url = new URL(urlPath);
+			URLConnection con = (URLConnection)url.openConnection();
+			InputStreamReader reader = new InputStreamReader(con.getInputStream(), "utf-8");
+			
+			BufferedReader buff = new BufferedReader(reader);
+			
+			while ((pageContents = buff.readLine()) != null) {
+				contents.append(pageContents);
+				contents.append("\r\n");
+			}
+			
+			buff.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return contents.toString();
 	}
 
 	@Override
 	public String getWotd() throws RemoteException {
-		// TODO Auto-generated method stub
-		return "Wotd";
+		String urlPath = xotd_url + "wotd.html";
+		String pageContents = "";
+		StringBuilder contents = new StringBuilder();
+
+		System.out.println("[RMI-hjgwak-server] URL : " + urlPath);
+		try {
+			URL url = new URL(urlPath);
+			URLConnection con = (URLConnection)url.openConnection();
+			InputStreamReader reader = new InputStreamReader(con.getInputStream(), "utf-8");
+			
+			BufferedReader buff = new BufferedReader(reader);
+			
+			while ((pageContents = buff.readLine()) != null) {
+				contents.append(pageContents);
+				contents.append("\r\n");
+			}
+			
+			buff.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return contents.toString();
 	}
 
 }
