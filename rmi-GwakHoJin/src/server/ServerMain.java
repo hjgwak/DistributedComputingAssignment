635268e0aea0e14eeb36b3642f832c945479d533
hjgwak <khj2830@gmail.com>
2016-11-08 00:14:39 +0900

    add automatic registry
    rmiregistry를 수동으로 하지 않아도 됨

--- rmi-GwakHoJin/src/server/ServerMain.java
+++ rmi-GwakHoJin/src/server/ServerMain.java
@@ -2,10 +2,12 @@ package server;
 
 import java.rmi.*;
 import java.rmi.server.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class ServerMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws RemoteException {
 		System.out.println("[RMI-hjgwak-Server] START");
 		// TODO Auto-generated method stub
 //		if (System.getSecurityManager() == null) {
@@ -13,15 +15,15 @@ public class ServerMain {
 //		}
 		
 		/*
-		 * RMI의 portNumber를 -p옵션을 통해 지정할 수 있도록 한다.
-		 * Allow portNumber for RMI by passing argument with -p option.
+		 * RMI의 portNumber를 -p옵션을 통해 지정할 수 있도록 한다. Default 1099
+		 * Allow portNumber for RMI by passing argument with -p option. Default 1099
 		 */
-		String portNumber = "";
+		int portNumber = 1099;
 		for (int i = 0; i < args.length; ++i) {
 			if (args[i].equals("-p")) {
 				try {
-					portNumber = args[i+1];
-					System.out.println("[RMI-hjgwak-Client] Specified Port Number : " + portNumber);
+					portNumber = Integer.parseInt(args[i+1]);
+					System.out.println("[RMI-hjgwak-Client] Specified Port Number : " + String.valueOf(portNumber));
 					break;
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -31,6 +33,8 @@ public class ServerMain {
 		
 		System.out.println("[RMI-hjgwak-Server] Try to bind xotd_service object.");
 		boolean bind_success = true;
+		Registry reg = LocateRegistry.createRegistry(portNumber);
+		
 		
 		/*
 		 * XotdServiceImpl 객체를 만들어서 rmi-hjgwak이라는 이름으로 바인딩 시킨다.
@@ -38,21 +42,19 @@ public class ServerMain {
 		 * Binding XotdServiceImpl object to registry named rmi-hjgwak
 		 * If portNumber was given through argument, binding to the passed port
 		 */
-		String url = "rmi://localhost";
-		if (!portNumber.equals("")) {
-			url += ":" + portNumber;
-		}
-		url += "/rmi-hjgwak";
+		String url = "rmi://localhost:" + String.valueOf(portNumber) + "/rmi-hjgwak";
 		try {
 			XotdServiceImpl xotd_service = new XotdServiceImpl();
-			Naming.rebind(url, xotd_service);
+			//Naming.rebind(url, xotd_service);
+			reg.rebind("rmi-hjgwak", xotd_service);
 			while(true);
 		} catch (Exception e) {
 			bind_success = false;
 			e.printStackTrace();
 		} finally {
 			try { 
-				Naming.unbind(url);
+				//Naming.unbind(url);
+				reg.unbind("rmi-hjgwak");
 				System.out.println("[RMI-hjgwak-Server] unbind xotd_service object.");
 			} catch (Exception e) {}
 		}
